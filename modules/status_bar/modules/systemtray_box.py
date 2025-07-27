593ec972b9e69be7004e67092038ad284345ecf5
maaru <maratarzymatov288@gmail.com>
2025-07-27 09:17:51 +0600

    close if unfocus

--- modules/status_bar/modules/systemtray_box.py
+++ modules/status_bar/modules/systemtray_box.py
@@ -2,19 +2,15 @@ from typing import Callable
 from fabric.widgets.wayland import WaylandWindow as Window
 from fabric.widgets.box import Box
 from fabric.widgets.button import Button
-from fabric.widgets.label import Label
 from .systemtray import SystemTray
-from gi.repository import Gtk, GdkPixbuf, Gdk  # type:ignore
+from gi.repository import Gtk, Gdk, GLib  # type:ignore
 from fabric.widgets.grid import Grid
+from fabric.hyprland.service import Hyprland, HyprlandEvent
+import json
 
 
 class TrayBox(Window):
-    def __init__(
-        self,
-        position: str,
-        bar_position: str,
-        do_click_handler: Callable,
-    ):
+    def __init__(self, position: str, bar_position: str, do_click_handler: Callable):
         self.position = position
         self.bar_position = bar_position
         self.do_click_handler = do_click_handler
@@ -28,37 +24,63 @@ class TrayBox(Window):
             v_align="fill",
             child=self._make_content(),
             margin=self.margin_handler(),
+            focusable=True,
         )
+
+        self.set_can_focus(True)
+        self.user_closed = False
+
+        self.connect("focus-out-event", self._on_focus_out)
+        self.connect("leave-notify-event", self._on_mouse_leave)
+        self.add_events(
+            Gdk.EventMask.FOCUS_CHANGE_MASK | Gdk.EventMask.LEAVE_NOTIFY_MASK
+        )
+        self._ignore_close_click = False
 
     def _make_content(self):
         main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
-
         content = Grid(name="tray-box", all_visible=True)
         tray = SystemTray()
-
         content.attach(tray, 0, 1, 1, 1)
         content.set_row_spacing(10)
 
         scrolled = Gtk.ScrolledWindow()
         scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.ALWAYS)
         scrolled.set_overlay_scrolling(False)
-        scrolled.add_with_viewport(content)
-        # scrolled.set_min_content_width(230)
+        scrolled.add(content)
         scrolled.set_min_content_height(100)
         content.set_size_request(190, 190)
 
         main_box.pack_start(scrolled, True, True, 0)
 
-        close_button = Gtk.Button(label="hide")
+        close_button = Gtk.Button(label="Close 🚪")
         close_button.connect("clicked", self._on_close_clicked)
+        self.close_button = close_button
         main_box.pack_start(close_button, False, False, 0)
 
         return main_box
 
     def _on_close_clicked(self, button=None, *args):
+        self.user_closed = True
         if self.do_click_handler:
             self.do_click_handler()
         self.hide()
+
+    def _on_focus_out(self, widget, event):
+        if not self._ignore_close_click:
+            self._ignore_close_click = True
+            self.close_button.emit("clicked")
+            GLib.timeout_add(100, self._reset_ignore_flag)
+
+    def _reset_ignore_flag(self):
+        self._ignore_close_click = False
+        return False
+
+    def _on_mouse_leave(self, widget, event):
+        if not self._ignore_close_click:
+            self._ignore_close_click = True
+            self.close_button.emit("clicked")
+            GLib.timeout_add(100, self._reset_ignore_flag)
 
     def margin_handler(self) -> str:
         pos = self.position
@@ -71,7 +93,6 @@ class TrayBox(Window):
                 return "30 0 0 0"
             elif pos == "top left":
                 return "30 0 0 220"
-
         elif barpos == "left":
             if pos == "top left":
                 return "120 30 0 0"
@@ -79,7 +100,6 @@ class TrayBox(Window):
                 return "0 0 0 30"
             elif pos == "bottom left":
                 return "0 0 120 30"
-
         elif barpos == "right":
             if pos == "top right":
                 return "120 30 0 0"
@@ -87,7 +107,6 @@ class TrayBox(Window):
                 return "0 0 0 30"
             elif pos == "bottom right":
                 return "0 30 120  0"
-
         elif barpos == "bottom":
             if pos == "bottom left":
                 return "0 0 30 120"
@@ -101,18 +120,31 @@ class TrayBox(Window):
 
 class TrayButtonHandler(Box):
     def __init__(
-        self,
-        tray_box_position: str,
-        bar_position: str,
-        orientation_pos: bool = True,
+        self, tray_box_position: str, bar_position: str, orientation_pos: bool = True
     ):
         super().__init__(
-            name="tray-handler-container",
-            orientation="h" if orientation_pos else "v",
+            name="tray-handler-container", orientation="h" if orientation_pos else "v"
         )
+        self.connection = Hyprland()
+        self.last_window_address = None
+        self.connection.connect("event::activewindow", self._on_active_window)
+
+        if self.connection.ready:
+            self._initialize_last_window()
+        else:
+            self.connection.connect(
+                "event::ready", lambda _: self._initialize_last_window()
+            )
 
         self.button = Button(
-            name="tray-handler-button", on_clicked=self.do_clicked, label=""
+            name="tray-handler-button",
+            h_align="fill",
+            v_align="fill",
+            all_visible=True,
+            h_expand=True,
+            v_expand=True,
+            on_clicked=self.do_clicked,
+            label="",
         )
 
         inner = Box(name="tray-handler-inner", all_visible=True)
@@ -129,18 +161,41 @@ class TrayButtonHandler(Box):
         )
         self.tray.hide()
 
+    def _initialize_last_window(self):
+        win_data = self.connection.send_command("j/activewindow").reply.decode()
+        win_json = json.loads(win_data)
+        self.last_window_address = win_json.get("address")
+
     def _on_box_click(self, widget, event):
         self.button.emit("clicked")
         return True
+
+    def _on_active_window(self, _, event: HyprlandEvent):
+        if len(event.data) < 3:
+            return
+
+        new_address = "0x" + event.data[2]
+        if new_address == self.last_window_address:
+            return
+
+        self.last_window_address = new_address
+
+        if self.tray.get_visible():
+            self.tray.user_closed = True
+            self.tray.hide()
+            self.button.set_label("")
 
     def do_clicked(self, button=None, *args):
         current = self.button.get_label()
         self.button.set_label("" if current == "" else "")
 
         if self.tray.get_visible():
+            self.tray.user_closed = True
             self.tray.hide()
         else:
+            self.tray.user_closed = False
             self.tray.show_all()
+            self.tray.grab_focus()
 
         return True
 
@@ -164,7 +219,11 @@ class SystemTrayHandler(Box):
         self.refresh_interval = refresh_interval
         self.spacing = spacing
 
-        super().__init__(name="systemtray-container", children=self._make_content())
+        super().__init__(
+            name="systemtray-container",
+            orientation="h" if orientation_pos else "v",
+            children=self._make_content(),
+        )
 
     def _make_content(self) -> TrayButtonHandler | SystemTray:
         if self.grid:
