f5680261918c804cf575c0dfafa3cb5929d03d73
maaru <maratarzymatov288@gmail.com>
2025-08-24 19:17:49 +0600

    update now can u change position icon

--- modules/status_bar/modules/title_window/windows_title.py
+++ modules/status_bar/modules/title_window/windows_title.py
@@ -1,14 +1,12 @@
 from config import RESOLVED_ICONS
 from utils import WINDOW_TITLE_MAP
-from typing import Optional
+from typing import Literal, Optional
 import unicodedata
 import re
 
 from fabric.widgets.image import Image
 from fabric.utils import truncate
 import gi
-from gi.repository import Gtk  # type: ignore
-from fabric.hyprland.widgets import ActiveWindow
 from fabric.widgets.box import Box
 from fabric.widgets.label import Label
 
@@ -28,21 +26,22 @@ class WindowsTitle(Box):
         title_exceptions: Optional[list] = None,
         vertical_title_length: int = 6,
         enable_icon: bool = True,
-        icon_resolve: bool = True,
-        icon_size: int = 16,
-        **kwargs,
+        resolve_icon: bool = True,
+        resolve_icon_size: int = 16,
+        resolve_position: Literal["left", "right"] = "left",
     ):
-        self.icon_size = icon_size
+        self.resolve_position = resolve_position
+        self.resolve_icon_size = resolve_icon_size
         self.title_exceptions = title_exceptions or []
-        self.icon_resolve = icon_resolve
+        self.resolve_icon = resolve_icon
+        self.enable_icon = enable_icon
         self.icon = IconResolver(RESOLVED_ICONS)
         self.is_horizontal = is_horizontal
+        self.vertical_title_length = vertical_title_length
         self.current_title = None
         self.truncation = truncation
         self.truncation_size = truncation_size
         self.title_map = title_map or []
-        self.enable_icon = enable_icon
-        self.vertical_title_length = vertical_title_length
         self.merged_titles = self.title_map + WINDOW_TITLE_MAP
 
         super().__init__(
@@ -50,7 +49,6 @@ class WindowsTitle(Box):
             h_expand=True,
             v_align="center",
             h_align="center",
-            **kwargs,
         )
 
         self.add(self._build_container("", ""))
@@ -74,32 +72,6 @@ class WindowsTitle(Box):
             None,
         )
 
-    def _build_container(self, win_class: str, win_title: str) -> Box:
-        container = Box(
-            name="statusbar-windows-title-box",
-            orientation="h" if self.is_horizontal else "v",
-        )
-
-        matched_exception = self._match_exception(win_class, win_title)
-
-        if matched_exception:
-            icon_text = matched_exception[1]
-            title_text = matched_exception[2]
-            if self.is_horizontal:
-                container.add(Label(f"{icon_text} {title_text}"))
-            else:
-                icon_text = self._trim_visual(icon_text, 1)
-                title_text = self._trim_visual(title_text, self.vertical_title_length)
-                container.add(Label(f"{icon_text}\n{title_text}"))
-            return container
-
-        if self.icon_resolve:
-            pixbuf = self.icon.get_icon_pixbuf(win_class, self.icon_size)
-            if pixbuf:
-                container.add(Image(pixbuf=pixbuf))
-        container.add(Label(self._get_title(win_title, win_class)))
-        return container
-
     def _trim_visual(self, text: str, max_width: int) -> str:
         result = ""
         current_width = 0
@@ -111,46 +83,67 @@ class WindowsTitle(Box):
             current_width += char_width
         return result
 
-    def _get_title(self, win_title: str, win_class: str) -> str:
-        if self.truncation:
-            win_title = truncate(win_title, self.truncation_size)
-
-        win_class_lower = win_class.lower()
+    def _get_title_and_icon(self, win_class: str, win_title: str):
         matched_exception = self._match_exception(win_class, win_title)
 
         if matched_exception:
-            title = matched_exception[2]
-            icon = matched_exception[1] if not self.icon_resolve else ""
-            if not self.enable_icon:
-                return title
-            if self.is_horizontal:
-                return f"{icon} {title}"
-            else:
-                title = self._trim_visual(title, self.vertical_title_length)
-                return f" {icon}\n{title}."
+            icon_text = matched_exception[1]
+            title_text = matched_exception[2]
+            if not self.is_horizontal:
+                icon_text = self._trim_visual(icon_text, 1)
+                title_text = self._trim_visual(title_text, self.vertical_title_length)
+            return title_text, icon_text
 
         matched_window = next(
-            (wt for wt in self.merged_titles if re.search(wt[0], win_class_lower)),
+            (wt for wt in self.merged_titles if re.search(wt[0], win_class.lower())),
             None,
         )
 
-        if not matched_window:
-            not_matched_window_icon = ""
-            not_matched_window = win_class_lower or "unknown"
-            if self.is_horizontal:
-                return f"{not_matched_window_icon} {not_matched_window}"
-            else:
-                not_matched_window = self._trim_visual(
-                    not_matched_window, self.vertical_title_length
-                )
-                return f" {not_matched_window_icon}\n{not_matched_window}."
-
-        title = matched_window[2]
-        icon = matched_window[1] if not self.icon_resolve else ""
-        if not self.enable_icon:
-            return title
-        if self.is_horizontal:
-            return f"{icon} {title}"
+        if matched_window:
+            icon_text = matched_window[1] if not self.resolve_icon else ""
+            title_text = matched_window[2]
         else:
-            title = self._trim_visual(title, self.vertical_title_length)
-            return f" {icon}\n{title}."
+            icon_text = "" if not self.resolve_icon else ""
+            title_text = win_class.lower() or "unknown"
+
+        if self.truncation and self.is_horizontal:
+            title_text = truncate(title_text, self.truncation_size)
+        elif not self.is_horizontal:
+            title_text = self._trim_visual(title_text, self.vertical_title_length)
+
+        if self.resolve_icon and self.enable_icon:
+            pixbuf = self.icon.get_icon_pixbuf(win_class, self.resolve_icon_size)
+            return title_text, pixbuf
+        else:
+            return title_text, icon_text
+
+    def _build_container(self, win_class: str, win_title: str) -> Box:
+        container = Box(
+            name="statusbar-windows-title-box",
+            orientation="h" if self.is_horizontal else "v",
+        )
+
+        text, icon_or_pixbuf = self._get_title_and_icon(win_class, win_title)
+
+        if isinstance(icon_or_pixbuf, str) and icon_or_pixbuf:
+            label = Label(text)
+            icon_label = Label(icon_or_pixbuf)
+            if self.resolve_position == "left":
+                container.add(icon_label)
+                container.add(label)
+            else:
+                container.add(label)
+                container.add(icon_label)
+        elif icon_or_pixbuf:
+            image = Image(pixbuf=icon_or_pixbuf)
+            label = Label(text)
+            if self.resolve_position == "left":
+                container.add(image)
+                container.add(label)
+            else:
+                container.add(label)
+                container.add(image)
+        else:
+            container.add(Label(text))
+
+        return container
