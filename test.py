3425f54f516a01e8d448f497bc455b31e33ace47
maaru <maratarzymatov288@gmail.com>
2025-09-01 15:54:16 +0600

    and other

--- test.py
+++ test.py
@@ -1,60 +1,73 @@
-from time import sleep
-from gi.repository import GLib
-from pydbus import SessionBus
-from threading import Thread
+from config import BATTERY_ICONS
+from utils import JsonManager
+from services import BatteryService, DeviceState
 
 
-class ScreenCaptureMonitor:
-    def __init__(self):
-        self.bus = SessionBus()
-        # правильный сервис и объект
-        self.portal = self.bus.get(
-            "org.freedesktop.portal.Desktop", "/org/freedesktop/portal/desktop"
-        )
-        self.is_capturing = False
-        self.callbacks = []
+class BatteryIcons:
+    def __init__(self, system_theme="dark"):
+        self.jsonc = JsonManager()
+        self.battery = BatteryService()
+        self.system_theme = system_theme
+        self.svg_icons_dir = list(BATTERY_ICONS.iterdir())
+        self.key = list(range(0, 101))
+        self.dir_names = {
+            "dark": ["battery_charging_dark", "battery_dark"],
+            "light": ["battery_charging_light", "battery_light"],
+        }
+        self.icon_dir = self._collect_icons()
+        self.ICONS = self.dict_icon()
 
-        # Подписка на DBus-сигналы Screencast
-        self.bus.subscribe(
-            iface="org.freedesktop.portal.ScreenCast",
-            signal="ScreencastStarted",
-            object="/org/freedesktop/portal/desktop",
-            signal_fired=self._handle_started,
-        )
+    def _collect_icons(self):
+        icons = []
+        for theme_dir in self.svg_icons_dir:
+            for d in self.dir_names.get(self.system_theme, []):
+                if theme_dir.name == d:
+                    icons.append(theme_dir)
+        return icons
 
-        self.bus.subscribe(
-            iface="org.freedesktop.portal.ScreenCast",
-            signal="ScreencastStopped",
-            object="/org/freedesktop/portal/desktop",
-            signal_fired=self._handle_stopped,
-        )
+    def dict_icon(self):
+        icons_dict = {}
 
-        # GLib loop в отдельном потоке
-        Thread(target=self._run_loop, daemon=True).start()
+        svg_dir = None
+        if self.icon_dir:
+            battery_state = self.battery.get_property("State")
+            state = DeviceState.get(int(battery_state or 0), "UNKNOWN")
 
-    def _run_loop(self):
-        loop = GLib.MainLoop()
-        loop.run()
+            if state == "CHARGING" and len(self.icon_dir) > 1:
+                svg_dir = self.icon_dir[1]
+            elif state == "DISCHARGING":
+                svg_dir = self.icon_dir[0]
+            else:
+                svg_dir = self.icon_dir[0]
 
-    def _handle_started(self, *args):
-        self.is_capturing = True
-        for cb in self.callbacks:
-            cb(True)
+        if svg_dir:
+            svg_files = {f.stem: f for f in svg_dir.glob("*.svg")}
 
-    def _handle_stopped(self, *args):
-        self.is_capturing = False
-        for cb in self.callbacks:
-            cb(False)
+            for k in self.key:
+                k_str = str(k)
+                if k_str in svg_files:
+                    icons_dict[k_str] = str(svg_files[k_str])
+                else:
+                    nearest = max(
+                        (
+                            int(name)
+                            for name in svg_files.keys()
+                            if name.isdigit() and int(name) <= k
+                        ),
+                        default=None,
+                    )
+                    if nearest is not None:
+                        icons_dict[k_str] = str(svg_files[str(nearest)])
+                    else:
+                        icons_dict[k_str] = str(svg_dir / "?.svg")
 
-    def subscribe(self, callback):
-        self.callbacks.append(callback)
+            icons_dict["?"] = str(svg_dir / "?.svg")
+        else:
+            icons_dict["?"] = None
+
+        return icons_dict
 
 
 if __name__ == "__main__":
-    while True:
-
-        def on_capture(status):
-            print("Экран записывается?", status)
-
-        monitor = ScreenCaptureMonitor()
-        monitor.subscribe(on_capture)
+    icons_dict = BatteryIcons().dict_icon()
+    print(JsonManager().dumps(icons_dict, indent=2))
