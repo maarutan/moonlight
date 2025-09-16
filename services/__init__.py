9fe4174bed37ded15e05f7e2e89c949a82143ac2
maaru <maratarzymatov288@gmail.com>
2025-09-16 20:09:18 +0600

    delete config check

--- services/__init__.py
+++ services/__init__.py
@@ -1,12 +1,10 @@
 from .player import MprisPlayer, MprisPlayerManager
-from .check_config import CheckConfig
 from .screen_record import ScreenRecorderService
 from .battery import BatteryService, DeviceState
 
 __all__ = [
     "MprisPlayer",
     "MprisPlayerManager",
-    "CheckConfig",
     "BatteryService",
     "DeviceState",
     "ScreenRecorderService",
